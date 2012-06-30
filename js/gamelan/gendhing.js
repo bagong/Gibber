b955da27e6ae3bd50b55313347aaf1edf6bfbf47
bagong <rs@bagong.de>
2012-06-30 23:23:26 +0200

    Keep gendhing.js as reference. It contains the scripts in load/gamelan/default-scripts

--- js/gamelan/gendhing.js
+++ js/gamelan/gendhing.js
@@ -1,7 +1,17 @@
-// fill arrays with parts, synths and useful info
-main = (function() {
-  // reset reference-arrays
-  parts = []; scales = []; synths = []; iNames = []; pNames = [];
+// GENDHING ####################################################################
+flags = {
+  gendhing : "wilujeng",
+  pathet : "p7",
+  form : "ladrang",
+  garap : "gerongan-salisir",
+  bukaInstr : "bonangBar",
+  gamelanName : "generic",
+  segment : "buka", // select segment to start with
+  irama : (this.form === "lancaran") ? "ir0": "irI", // select irama to start with - adjust speed (G.setBPM(60)) if you want to start in irII
+  bpm : 120
+};
+startEngines = (function() {
+  scales = []; iNames = []; pNames = [];
   var ensemble = now.instruments;
   var ix = 0;
   for (var iName in ensemble) {
@@ -11,30 +21,28 @@ main = (function() {
       iNames.push(iName);
       if (iName===flags.bukaInstr) { window["ixBukaInstr"] = ix; }
       if (iName===conf.timeKeeper) { window["ixTimeKeeper"] = ix; }
-      switch (true) {
-        case iName === "kendhang" : synths.push(Kendhang()); break;
-        case conf.synthPref === "light" : synths.push(Synth()); break;
-        default : synths.push(instr.ugen);
-      }
       scales.push((instr.frequency) ? instr.frequency : instrFrequencies(instr));
       for (var pName in nyaga) {
         if (nyaga[pName].ricikan === iName) { pNames.push(pName); break; }
-      }      
-      if (instr.effectBus) {
-        synths[ix].send(instr.effectBus[0],instr.effectBus[1]);
       }
       ix++;
     }
   }
   instrCnt = ix;
-  // push part-definitions to array "parts" ######################################
+  parts = []; synths = [];
+  for (var i=0;i<instrCnt;i++) {
+  // create Synth-Dummies
+    synths.push(Synth()); // synths.push(Synth({ active : false }).out())
+  //  if (instr.effectBus) { synths[ix].send(instr.effectBus[0],instr.effectBus[1]); }
+  }
+  // push part-definitions to array "parts" ####################################
   partsGen();
   // generate SHORTHANDS for found parts #######################################
-  for (i in iNames) {
-    var name = iNames[i];
+  for (var i=0;i<instrCnt;i++) {
+    var iName = iNames[i];
     window["p"+i] = parts[i];
     window["s"+i] = synths[i];
-    switch (name) {
+    switch (iName) {
       case "bonangPan" : bp = parts[i]; sbp = synths[i]; break;
       case "bonangBar" : bb = parts[i]; sbb = synths[i]; break;
       case "peking" : pk = parts[i]; spk = synths[i]; break;
@@ -50,14 +58,10 @@ main = (function() {
       case "gong" : ga = parts[i]; sga = synths[i]; break;
       case "kendhang" : kd = parts[i]; skd = synths[i];
     };
-    if (name===flags.bukaInstr) { bi = parts[i]; sbi = synths[i]; };
-    if (name===conf.timeKeeper) { tk = parts[i]; stk = synths[i]; };
+    if (iName===flags.bukaInstr) { bi = parts[i]; sbi = synths[i]; };
+    if (iName===conf.timeKeeper) { tk = parts[i]; stk = synths[i]; };
   }
-  return "scales, synths, parts, iNames (instruments), pNames (persons) and busses prepared..." // To play bonang barung type bb.play(). To play everyting type gendhing.play()";
-}());
-
-gendhing = (function() {
-// PLAYER Convenience ##########################################################
+  // PLAYER Convenience ##########################################################
   doForAll = function(action,counter) {
     counter = counter || tk.counter || 1;
     var partsCnt = parts.length;
@@ -72,20 +76,24 @@ gendhing = (function() {
         parts[i].pause();
       } else if (action === "stop") {
         parts[i].stop();
-      } else if (action === "kill") { // kill does not make much sense as you can't restart yet
+      } else if (action === "kill") { // kill does not make much sense as you can\'t restart yet
         parts[i].kill();
       }
     }
-    var result = "Action '"+action+"' executed for all parts";
+    var result = "Action \'"+action+"\' executed for all parts";
     return result;
   }
-  play = function(counter) { doForAll("play",counter); if ( toggle.logBranching && flags.segment === "buka") { console.log(msg.console.onStartPlaying); } return msg.returnInfo.allPlay(); };
-  once = function(counter) { doForAll("once",counter); return msg.returnInfo.allOnce() };
-  pause = function(counter) { doForAll("pause",counter); return msg.returnInfo.allPause() };
-  stop = function(counter) { doForAll("stop",counter); return msg.returnInfo.allStop() };
-  kill = function(counter) { doForAll("kill",counter); return msg.returnInfo.allKill() };
-  // playBuka = function() { for (var i=0;i<instrCnt;i++) { parts[i] = partGen(i,"buka","irI",true); } };
+  gendhing = {
+    play : function(counter) { doForAll("play",counter); if ( toggle.logBranching && flags.segment === "buka") { console.log(msg.console.onStartPlaying); } return msg.returnInfo.allPlay(); },
+    once : function(counter) { doForAll("once",counter); return msg.returnInfo.allOnce() },
+    pause : function(counter) { doForAll("pause",counter); return msg.returnInfo.allPause() },
+    stop : function(counter) { doForAll("stop",counter); return msg.returnInfo.allStop() },
+    kill : function(counter) { doForAll("kill",counter); return msg.returnInfo.allKill() }
+  }
+  return "scales, synths, parts, iNames (instruments), pNames (persons) and busses prepared..." // To play bonang barung type bb.play(). To play everyting type gendhing.play()";
+}());
 
+sequencers = (function() {
 // #############################################################################
   var ppGongan = now.pp.gongan;
   var ppBuka = now.pp.buka;
@@ -116,7 +124,7 @@ gendhing = (function() {
     nthGong++;
     if (toggle.logGongan) { console.log(msg.console.onGong()); }
     nthNong = 1; nthPul = 0; nthThuk = 0; nthPyang = 0; // reset relative counters
-    nthGatra = 1; nthSabet = 0; keteg = 1; // rethink exactly whether to reset pulse to 0 manually or not
+    nthGatra = 1; nthSabet = 0; keteg = 1; // rethink exactly whether to reset gPulse to 0 manually or not
     kar.branching(); // trigger settings for gongan to come
     fullPeriod = now.pp.gongan; // reset orientational variables in case gongan changed
     halph = fullPeriod/2;
@@ -131,8 +139,8 @@ gendhing = (function() {
     Seq({
       active : false,
       function : [ function() {
-        pulse = parts[ixTimeKeeper].counter; // reset to 0 on each stop()
-        if (pulse % now.pp.keteg===0) { keteg++ }; // keteg is useful but not used yet
+        gPulse = parts[ixTimeKeeper].counter; // reset to 0 on each stop()
+        if (gPulse % now.pp.keteg===0) { keteg++ }; // keteg is useful but not used yet
         ticker++
       }],
       durations : [_32]
@@ -145,39 +153,39 @@ gendhing = (function() {
       function : [
         function() {
           // sabet/pyang: ------------------------------------------------------
-          if (pulse % _32nd === 0) {
+          if (gPulse % _32nd === 0) {
             nthSabet++
-            if (pulse % _16th !==0) {
+            if (gPulse % _16th !==0) {
               nthPyang++;
               if (toggle.logGongan) {console.log(msg.console.onKempyang());}
             }
             // tiba/thuk: ------------------------------------------------------
-            if (pulse % _16th === 0) {
-              if (pulse===ppBuka && flags.segment === "buka") { onGong(); }
-              if (pulse % _8th !== 0) {
+            if (gPulse % _16th === 0) {
+              if (gPulse===ppBuka && flags.segment === "buka") { onGong(); }
+              if (gPulse % _8th !== 0) {
                 nthThuk++;
                 if (toggle.logGongan) {console.log(msg.console.onKethuk());}
               }
               // gatra/pul: ----------------------------------------------------
-              if (pulse % _8th === 0) {
-                if ((pulse % quarter !== 0) && (pulse !== ppGatra)) {
+              if (gPulse % _8th === 0) {
+                if ((gPulse % quarter !== 0) && (gPulse !== ppGatra)) {
                   nthPul++;
                   if (toggle.logGongan) {console.log(msg.console.onKempul());}
                   if (toggle.logGongan) {console.log("\n");}
-                } else if (pulse === ppGatra) {
+                } else if (gPulse === ppGatra) {
                   if (toggle.logGongan) {console.log(msg.console.onWela()); }
                   if (toggle.logGongan) {console.log("\n"); }
                 }
-                if (pulse % quarter === 0) {
+                if (gPulse % quarter === 0) {
                   // Here go things on kenong-level
-                  if ((pulse % fullPeriod !== 0) && (ticker !== ppBuka)) {
+                  if ((gPulse % fullPeriod !== 0) && (ticker !== ppBuka)) {
                     if (toggle.logGongan) { console.log( msg.console.onKenong()); }
                     if (toggle.logGongan) { console.log("\n"); }
                     nthNong++;
                   }
-                  if (pulse % halph === 0) {
+                  if (gPulse % halph === 0) {
                     // Here go things on halph-gong-level
-                    if (pulse % fullPeriod === 0) {
+                    if (gPulse % fullPeriod === 0) {
                       onGong();
                     }
                   }
@@ -209,7 +217,7 @@ gendhing = (function() {
         // speed-change
         if (flags.seseg || flags.rem) { kar.seseg(); }
         // invoke ngelik-signal
-        if ( !toggle.autoPilot && flags.goToNgelik && pulse === decideUponNgelikBranch) {
+        if ( !toggle.autoPilot && flags.goToNgelik && gPulse === decideUponNgelikBranch) {
             partSet(ixBonangPan,flags.segment,flags.irama);
             partSet(ixBonangBar,flags.segment,flags.irama);
             console.log(msg.console.onWatchDogTriggerNgelikSignal)
@@ -220,43 +228,43 @@ gendhing = (function() {
           if (toggle.logWatchDogs) { console.log(msg.console.onWatchDogToSteadySpeed()); }
         }
         // iramaSwitch up (slow->fast)
-        if (flags.seseg && !flags.doSuwuk && flags.irama === "irII" && pulseUnit < par.thresholdTanggung && pulse % favoredIramaSwitch === 0 ) {
+        if (flags.seseg && !flags.doSuwuk && flags.irama === "irII" && pulseUnit < par.thresholdTanggung && gPulse % favoredIramaSwitch === 0 ) {
           kar.irama("irI");
           if (toggle.logWatchDogs) { console.log(msg.console.onWatchDogToIrI()); }
         }
-        if (flags.seseg && !flags.doSuwuk && flags.irama === "irI" && pulseUnit < (par.thresholdTanggung-par.bufferTanggung) && pulse % favoredReturnToSteadyIrI === 0 ) {
+        if (flags.seseg && !flags.doSuwuk && flags.irama === "irI" && pulseUnit < (par.thresholdTanggung-par.bufferTanggung) && gPulse % favoredReturnToSteadyIrI === 0 ) {
           kar.jadi();
           if (toggle.logWatchDogs) { console.log(msg.console.onWatchDogToSteadySpeed()); }
         }
         // iramaSwitch down (fast->slow)
-        if (flags.rem && !flags.doSuwuk && flags.irama === "irI" && pulseUnit > (par.thresholdDadi) && (pulse % favoredIramaSwitch === 0) ) {
+        if (flags.rem && !flags.doSuwuk && flags.irama === "irI" && pulseUnit > (par.thresholdDadi) && (gPulse % favoredIramaSwitch === 0) ) {
           kar.irama("irII");
           if (toggle.logWatchDogs) { console.log(msg.console.onWatchDogToIrII()); }
         }
-        if (flags.rem && !flags.doSuwuk && flags.irama === "irII" && pulseUnit > (par.thresholdDadi+par.bufferDadi) && (pulse % favoredReturnToSteadyIrII === 0) ) {
+        if (flags.rem && !flags.doSuwuk && flags.irama === "irII" && pulseUnit > (par.thresholdDadi+par.bufferDadi) && (gPulse % favoredReturnToSteadyIrII === 0) ) {
           kar.jadi();
           if (toggle.logWatchDogs) { console.log(msg.console.onWatchDogToSteadySpeed()); }
         }
         // suwuk (needs much work)
         if (flags.doSuwuk) {
           switch (true) {
-            case pulse === startSuwukPossibleMin || ((pulse === startSuwukPossibleMax) && !flags.isGonganSuwuk) :
+            case gPulse === startSuwukPossibleMin || ((gPulse === startSuwukPossibleMax) && !flags.isGonganSuwuk) :
               partSet(ixKendhang,"suwuk"); // there will be more going on in a nice suwuk
               kar.nyeseg(10);
               flags.isGonganSuwuk = true; // for now suwuks that span one gongan
               flags.ngampat = true;
               if (toggle.logWatchDogs) { console.log(msg.console.onWatchDogReinforceSuwukMode); }
               break;
-            case ((pulse === startSuwukPossibleMinEndNgampat) && flags.ngampat) || ((pulse === startSuwukPossibleMaxEndNgampat) && flags.ngampat && !flags.habisNgampat) :
+            case ((gPulse === startSuwukPossibleMinEndNgampat) && flags.ngampat) || ((gPulse === startSuwukPossibleMaxEndNgampat) && flags.ngampat && !flags.habisNgampat) :
               kar.jadi();
               flags.habisNgampat = true;
               if (toggle.logWatchDogs) { console.log(msg.console.onWatchDogToSteadySpeed()); }
               break;
-            case pulse === favoredFinalSlowDown :
+            case gPulse === favoredFinalSlowDown :
               kar.nyuwuk();
               if (toggle.logWatchDogs) { console.log(msg.console.onWatchDogToFinalSlowdown); }
               break;
-            case flags.isGonganSuwuk === true && pulse === 1 &&
+            case flags.isGonganSuwuk === true && gPulse === 1 &&
               ((flags.irama === "irI" && pulseUnit > par.suwukThresholdIrI) || (flags.irama === "irII" && pulseUnit > par.suwukThresholdIrII)) :
               kar.gongSuwuk();
               if (toggle.logWatchDogs) { console.log(msg.console.onGongSuwuk); }
@@ -280,18 +288,17 @@ gendhing = (function() {
         active : false,
         function : [ function() {
         switch (true) {
-          case nthGong === 0 && pulse === startSlowDownDuringBuka : kar.ngerem(1.5); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSlowDown()); } break;
-          case nthGong === 1 && pulse === stopSlowDownAfterBuka : kar.jadi(); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSteadySpeed()); } break;
-          case nthGong === 2 && pulse === iramaDownIn1stKenongan : kar.ngerem(2.5); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSlowDown()); } break;
-          case nthGong === 4 && pulse === iramaUpIn3rdKenongan : kar.nyeseg(); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSpeedUp()); } break;
-          case nthGong === 6 && pulse === iramaDownIn3rdKenongan : kar.ngerem(); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSlowDown()); } break;
-          case nthGong === 7 && pulse === startSuwukMode : flags.doSuwuk = true; if (toggle.logAutoPilot) { console.log(msg.console.onAutoSetSuwukFlag); } break;
+          case nthGong === 0 && gPulse === startSlowDownDuringBuka : kar.ngerem(1.5); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSlowDown()); } break;
+          case nthGong === 1 && gPulse === stopSlowDownAfterBuka : kar.jadi(); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSteadySpeed()); } break;
+          case nthGong === 2 && gPulse === iramaDownIn1stKenongan : kar.ngerem(2.5); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSlowDown()); } break;
+          case nthGong === 4 && gPulse === iramaUpIn3rdKenongan : kar.nyeseg(); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSpeedUp()); } break;
+          case nthGong === 6 && gPulse === iramaDownIn3rdKenongan : kar.ngerem(); if (toggle.logAutoPilot) { console.log(msg.console.onAutoSlowDown()); } break;
+          case nthGong === 7 && gPulse === startSuwukMode : flags.doSuwuk = true; if (toggle.logAutoPilot) { console.log(msg.console.onAutoSetSuwukFlag); } break;
         }
       }],
       durations : [_32] })
     )
     autoPilot = ap = parts[parts.length-1];
   }
-  return "... welcome to gendhing. You are almost there. Type play() or make your choices first. (not much for now, though ;))";
+  return "... welcome to gendhing. You are almost there. Type gendhing.play() or make your choices first. (not much for now, though ;))";
 }());
-
